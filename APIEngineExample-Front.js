a2285c36276a819d344b5b3519b93a1af4e70d45
Parna Asadi <70844560+Perriex@users.noreply.github.com>
2021-10-28 11:28:52 +0330

    Create APIEngineExample-Front.js

---
+++ APIEngineExample-Front.js
@@ -0,0 +1,56 @@
+import { TokenManager } from "src/Identity/Service/TokenManager";
+const { GetToken } = TokenManager();
+const secret = "AMP_!YUHDSJHYG@&12312!W@sAs";
+
+export const BaseUrl = process.env.REACT_APP_API_BASE;
+const GetData = (url) => {
+  return fetch(BaseUrl + url, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + GetToken(),
+    },
+  }).then((res) => res.json());
+};
+
+const PostData = async (url, body) => {
+  const data = await fetch(BaseUrl + url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      Authorization: "Bearer " + GetToken(),
+    },
+    body: JSON.stringify(body),
+  });
+
+  const json = await data.json();
+
+  if (data.status < 400) return json;
+  throw json;
+};
+
+const UploadFileRequest = async (file) => {
+  const form = new FormData();
+  form.append("file", file);
+  form.append("secret", secret);
+  console.log(GetToken());
+  const init = {
+    headers: {
+      Authorization: "Bearer " + GetToken(),
+    },
+    method: "POST",
+    body: form,
+  };
+
+  const res = await fetch(BaseUrl + "File/Upload", init);
+  try {
+    const json = await res.json();
+    if (res.status < 400) return json;
+    throw json;
+  } catch {
+    return null;
+  }
+};
+
+const GetFileDownloadLink = (hash) => BaseUrl + "File/Download/" + hash;
+
+export { PostData, GetData, UploadFileRequest, GetFileDownloadLink };
